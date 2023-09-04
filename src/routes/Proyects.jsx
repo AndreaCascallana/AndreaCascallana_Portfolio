fe64a65e727518d089c88e06c8c8f47a75efdd68
Andrea Cascallana Rodríguez <acascallana3016@esdmadrid.es>
2023-09-04 14:13:35 +0200

    prueba

--- src/routes/Proyects.jsx
+++ src/routes/Proyects.jsx
@@ -15,21 +15,21 @@ const Proyects = () => {
           <ProyectSingle
             name="PLICOGA"
             picture="/plicoga.png"
-            alt="plicoga proyect"
+            alt="plicoga"
             description="Plataforma integral de inspección, conservación, y control del alumbrado urbano y galerías subterráneas."
             url="https://www.figma.com/proto/flIm7U52v8NEzvN0bf4aWr/PLICOGA_Portfolio-AndreaCascallana?page-id=601%3A12980&type=design&node-id=604-14990&viewport=-2227%2C6241%2C1.04&t=wbfrA5xHUsyl39Vy-1&scaling=contain&mode=design"
           />
           <ProyectSingle
             name="CONNECTO"
             picture="/connecto.png"
-            alt="connecto proyect"
+            alt="connecto"
             description="App que busca generar una red de apoyo para las familias que acuden a Centros de Atención Temprana."
             url="https://www.figma.com/proto/F7ZL5ND8PuON9L3haOfDZj/CONNECTO_Portfolio-AndreaCascallana?page-id=1202%3A61599&type=design&node-id=1202-61867&viewport=471%2C729%2C0.48&t=jVhyr5zmLLbnhU9s-1&scaling=contain&mode=design"
           />
           <ProyectSingle
             name="KEY"
             picture="/key.png"
-            alt="key proyect"
+            alt="key"
             description="Key es una plataforma digital de oferta y demanda de espacios de trabajo específicos para perfiles profesionales creativos."
             url="https://www.figma.com/proto/vBGOQlWlTbW4zwxav7R333/KEY_Portfolio-AndreaCascallana?page-id=1701%3A43853&type=design&node-id=1702-44740&viewport=490%2C772%2C1.04&t=0Hw1QT2iagNp9Arc-1&scaling=contain&mode=design"
           />
