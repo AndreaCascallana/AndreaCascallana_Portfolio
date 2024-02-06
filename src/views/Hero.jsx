7a5e070b156251f4c7d991d7da19ab5a3d64ad1d
Andrea Cascallana Rodríguez <acascallana3016@esdmadrid.es>
2024-02-07 00:10:15 +0100

    add behance proyects

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="hero-container px-8 lg:px-0 pt-6 lg:pt-8 pb-16 flex flex-col justify-center lg:flex-row items-center gap-4 h-full">
-      <a className="img-container lg:h-fit" target="_blank" href="https://www.figma.com/proto/flIm7U52v8NEzvN0bf4aWr/PLICOGA_Portfolio-AndreaCascallana?page-id=601%3A12980&type=design&node-id=604-14990&viewport=-2227%2C6241%2C1.04&t=wbfrA5xHUsyl39Vy-1&scaling=contain&mode=design">
+      <a className="img-container lg:h-fit" target="_blank" href="https://www.behance.net/gallery/190887711/PLICOGA">
         <img className="w-full h-full object-contain rounded-md" src="/plicoga.png" alt="plicoga" />
       </a>
-      <a className="img-container lg:h-fit" target="_blank" href="https://www.behance.net/gallery/190737511/Key-Platform-Visual-Design">
+      <a className="img-container lg:h-fit" target="_blank" href="https://www.behance.net/gallery/190737511/Key">
         <img className="w-full h-full object-contain rounded-md" src="/key.png" alt="key" />
       </a>
     </div>
